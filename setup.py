a51482096a73f2ad3214cf9c04252ebf5dadb3fe
Artin <lengthmin@gmail.com>
2019-10-22 16:12:35 +0800

    release: 1.2.7

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class PyTest(TestCommand):
 
 
 setuptools.setup(name="auth_swust",
-                 version="1.2.6",
+                 version="1.2.7",
                  url='https://github.com/BuddingLab/auth_swust',
                  author="BuddingLab",
                  author_email="admin@maxlv.org,",
