727e661c8d01392755fc07173cbcd6bf0b923d79
Artin <lengthmin@gmail.com>
2020-02-23 00:42:36 +0800

    release: v1.3.0

--- setup.py
+++ setup.py
@@ -9,33 +9,36 @@ with open("requirements.txt") as f:
 class PyTest(TestCommand):
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = ['tests/']
+        self.test_args = ["tests/"]
         self.test_suite = True
 
     def run_tests(self):
         # import here, cause outside the eggs aren't loaded
         import pytest
+
         errno = pytest.main(self.test_args)
         sys.exit(errno)
 
 
-setuptools.setup(name="auth_swust",
-                 version="1.2.8",
-                 url='https://github.com/BuddingLab/auth_swust',
-                 author="BuddingLab",
-                 author_email="admin@maxlv.org,",
-                 description="auth_swust",
-                 packages=setuptools.find_packages(),
-                 install_requires=install_requires,
-                 classifiers=[
-                     "Programming Language :: Python :: 3.6",
-                     "Programming Language :: Python :: 3.7",
-                     "License :: OSI Approved :: MIT License",
-                     "Operating System :: OS Independent",
-                 ],
-                 package_data={
-                     'auth_swust': [
-                         'captcha_recognition/model/*.pth',
-                         'captcha_recognition/model/*.model'
-                     ],
-                 })
+setuptools.setup(
+    name="auth_swust",
+    version="1.3.0",
+    url="https://github.com/BuddingLab/auth_swust",
+    author="BuddingLab",
+    author_email="admin@maxlv.org,",
+    description="auth_swust",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={
+        "auth_swust": [
+            "captcha_recognition/model/*.pth",
+            "captcha_recognition/model/*.model",
+        ]
+    },
+)
