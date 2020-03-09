c570d6a02d2c82e8a1fa048b63e6e0636f757f69
Artin <lengthmin@gmail.com>
2020-03-09 16:39:13 +0800

    fix: update index url

--- auth_swust/constants.py
+++ auth_swust/constants.py
@@ -2,7 +2,7 @@ class URL:
     auth_server_url = f"http://cas.swust.edu.cn/authserver/login?service="
     index_url = (
         auth_server_url
-        + "http://my.swust.edu.cn/mht_shall/a/service/serviceFrontManage/cas"
+        + "http://myo.swust.edu.cn/mht_shall/a/service/serviceFrontManage/cas"
     )
     jwc_auth_url = (
         auth_server_url
@@ -12,4 +12,4 @@ class URL:
     syk_auth_url = auth_server_url + "http://202.115.175.177/swust/"
     captcha_url = f"http://cas.swust.edu.cn/authserver/captcha"
     get_key_url = f"http://cas.swust.edu.cn/authserver/getKey"
-    student_info_url = f"http://my.swust.edu.cn/mht_shall/a/service/studentInfo"
+    student_info_url = f"http://myo.swust.edu.cn/mht_shall/a/service/studentInfo"
